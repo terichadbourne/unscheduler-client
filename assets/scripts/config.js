5af4cb2d7e78e442c4ac1f34b41eac24f49ccabd
terichadbourne <terichad@gmail.com>
2018-06-28 10:50:12 -0400

    Remove extra / from production URL
    - Remove / from end of production URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://unscheduler-api.herokuapp.com/',
+  production: 'https://unscheduler-api.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
